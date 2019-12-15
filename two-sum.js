929051395cd3a232bef0f8d8697ed7bb2e0912a6
Joseph Arias <arias.joseph.a@gmail.com>
2019-12-15 10:46:10 -0500

    two sum

--- two-sum.js
+++ two-sum.js
@@ -8,3 +8,20 @@
 
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1]
+
+var twoSum = function(nums, target) {
+  const hash = {}
+  for(let i = 0; i < nums.length; i++) {
+    let currentNumber = nums[i];
+    let neededValue = target - currentNumber;
+    let index2 = hash[neededValue]
+    if(index2 != null) {
+      return [index2, i]
+    } else {
+      hash[currentNumber] = i
+    }
+
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
