766e9b8ccf056b7a9cc965595833995bec6408fb
Salsa-Dude <arias.joseph.a@gmail.com>
2020-01-09 11:52:57 -0500

    added another solution

--- remove-element/solution.js
+++ remove-element/solution.js
@@ -12,3 +12,16 @@ var removeElement = function(nums, val) {
 };
 
 removeElement([3,2,2,3], 3)
+
+// Another way without using splice
+// var removeElement = function(nums, val) {
+//     let i = 0;
+//      for (let j = 0; j < nums.length; j++) {
+//          if (nums[j] != val) {
+//              // nums[i] = nums[j];
+//              i++;
+//          }
+//      }
+//      return i;
+//  };
+ 
